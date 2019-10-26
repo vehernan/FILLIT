dfa8d231476beabd601b9f696aab611d992c0544
Veneranda Hernandez <46582053+vehernan@users.noreply.github.com>
2019-10-25 23:55:07 -0700

    Add files via upload

---
+++ Fillit/fillit.h
@@ -0,0 +1,20 @@
+#ifndef FILLIT_H
+# define FILLIT_H
+
+#include "libft/libft.h"
+#include "libft/get_next_line.h"
+
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+
+
+#define BUFFER_SIZE 546
+#define TETROMINOVAL 20
+#define MAXAMOUNT 26
+
+
+#endif
