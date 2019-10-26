7d8e3fa70185ca33a3ae33abff918c4605ad83ef
vehernan <hveneranda@yahoo.com>
2019-10-26 00:10:24 -0700

    deleted library

--- Fillit/fillit.c
+++ Fillit/fillit.c
@@ -1,17 +1,3 @@
-#include <unistd.h>
-#include <stdlib.h>
-#include <string.h>
-#include <fcntl.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-
-#define BUFFER_SIZE 546
-#define TETROMINOVAL 20
-#define MAXAMOUNT 26
-
-#include "fillit.h"
-#include "libft/libft.h"
-#include "libft/get_next_line.h"
 
 // utils.c
 
